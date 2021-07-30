61a2494e40a67b36cba8e8c4d8ddaa2cb44ffd26
Thomas Guettler <guettliml@thomas-guettler.de>
2021-07-30 06:34:21 +0200

    typo

--- release.py
+++ release.py
@@ -5,14 +5,16 @@ import sys
 
 def call(cmd, capture_output=False):
     if isinstance(cmd, str):
-        cmd = cmd.split()  # no user-input will come here. This is safe.
+        cmd = cmd.split()
     print(cmd)
     result = subprocess.run(cmd, capture_output=capture_output)
     if result.returncode == 0:
         print('    ok')
         return result
-    print(result.stdout)
-    print(result.stderr)
+    if result.stdout:
+        (result.stdout)
+    if result.stderr:
+        print(result.stderr)
     print(f'failed. {result.returncode}')
     sys.exit()
 
@@ -26,15 +28,14 @@ def check_git_is_clean():
 
 
 def check_tests_and_coverage():
-    call(
-        'pytest --cov-report=html --no-cov-on-fail --cov --cov-fail-under=75 --create-db --exitfirst'.split())
+    call('pytest --cov-report=html --no-cov-on-fail --cov --cov-fail-under=75 --create-db --exitfirst')
 
 
 def main():
     os.chdir(os.path.join(os.getenv('VIRTUAL_ENV'), 'src/django-trace-cockpit'))
     check_git_is_clean()
     check_tests_and_coverage()
-    call(['git', 'push'])
+    call('git push')
     call('python setup.py sdist')
     call('pip install twine')
     call('twine upload dist/*')
