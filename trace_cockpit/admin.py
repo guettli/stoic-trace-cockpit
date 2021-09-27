efbe18e56086707493460fb055f0100dfbdd5384
Thomas Guettler <guettliml@thomas-guettler.de>
2021-09-27 22:07:41 +0200

    implemented start-page. And link for config to logs.
    closes #3

--- trace_cockpit/admin.py
+++ trace_cockpit/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.urls import reverse
 from django.utils.html import format_html
 from ordered_model.admin import OrderedModelAdmin
 
@@ -10,7 +11,7 @@ from trace_cockpit.models import TraceConfig, TraceLog
 
 class TraceConfigAdmin(OrderedModelAdmin):
     list_display = ['name', 'is_active', 'move_up_down_links']
-    readonly_fields = ['hint']
+    readonly_fields = ['hint', 'link_to_logs']
 
     def hint(self, config: TraceConfig):
         if not config.is_active:
@@ -20,7 +21,17 @@ class TraceConfigAdmin(OrderedModelAdmin):
             return format_html('''
              <div style="color: red">Config "{first}" is active and has a higher order. The above config will only
              be called if the higher config does not match to the request.</div>''', first=first)
-        return ''
+        return 'Everything looks fine'
+
+    def link_to_logs(self, config: TraceConfig):
+        if not config.id:
+            return ''
+        count = TraceLog.objects.filter(config=config).count()
+        if not count:
+            return 'There are no logs for this config yet'
+        return format_html('<a href="{}?config__id__exact={}">{} {} Logs</a>',
+                           reverse('admin:trace_cockpit_tracelog_changelist'), config.id, count,
+                           config.name)
 
 
 admin.site.register(TraceConfig, TraceConfigAdmin)
@@ -32,6 +43,11 @@ class TraceLogAdmin(ModelAdmin):
                        'skipped_events_because_max_reached_html',
                        'skipped_modules_html',
                        ]
+
+    list_filter = (
+        'config',
+        'datetime_created',
+        'success')
 
     def html(self, log):
         return jsontrace_to_html(log.json_lines.splitlines())
@@ -45,5 +61,6 @@ class TraceLogAdmin(ModelAdmin):
         return count_dict_to_html(log.skipped_events_because_max_reached)
 
     skipped_events_because_max_reached_html.short_description = 'Skipped events, because max reached'
+
 
 admin.site.register(TraceLog, TraceLogAdmin)
--- trace_cockpit_testsite/urls.py
+++ trace_cockpit_testsite/urls.py
@@ -1,28 +1,33 @@
-"""trace_cockpit_testsite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, reverse
+from django.utils.html import format_html
+
 
 def dummy_view(request):
     i = 0
     i += 1
     return HttpResponse(f'<html><body>i: {i}</body></html>')
 
+def start_page(request):
+    return HttpResponse(format_html('''
+    <html>
+     <h1>Stoic Trace Cockpit</h1>
+     Thank you for choosing stoic trace cockpit!
+     
+     <p>
+      Configuring the trace cockpit gets done via the <a href="{link_to_admin}">Admin</a>.
+     </p>
+     
+     <p>
+      I like feedback! Is there something which is not working? What could be improved?
+      Please provide your feedback via the 
+      <a href="https://github.com/guettli/stoic-trace-cockpit/issues">Github Issue Tracker</a>.
+    </p>''', link_to_admin=reverse('admin:app_list', args=('trace_cockpit',))))
+     
+     
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dummy_view', dummy_view, name='dummy_view'),
+    path('', start_page, name='start_page'),
 ]
